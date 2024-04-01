4c45534ad7ece802ec2e6f994840b614e5471d21
Atakan <atakandogan2001@gmail.com>
2024-04-01 12:07:47 +0300

    Theme change

--- suntekstilwebapp/lib/main.dart
+++ suntekstilwebapp/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:suntekstilwebapp/src/components/sidebar.dart'; 
 import 'package:suntekstilwebapp/src/pages/HomePage.dart'; 
 import 'package:suntekstilwebapp/src/pages/SettingsPage.dart'; 
+import 'package:suntekstilwebapp/src/theme/theme_provider.dart'; 
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,17 +11,25 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Sidebar Örneği',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider<ThemeProvider>(
+      create: (_) => ThemeProvider(), // ThemeProvider örneği oluşturun
+      child: Consumer<ThemeProvider>(
+        builder: (context, themeProvider, _) {
+          return MaterialApp(
+            title: 'Flutter Sidebar Örneği',
+            theme: themeProvider.getThemeData(context),
+            darkTheme: themeProvider.getThemeData(context).copyWith( 
+              brightness: Brightness.dark,
+            ),
+            themeMode: ThemeMode.system,
+            initialRoute: '/',
+            routes: {
+              '/': (context) => HomePage(),
+              '/settings': (context) => SettingsPage(),
+            },
+          );
+        },
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => HomePage(),
-        '/settings': (context) => SettingsPage(),
-      },
     );
   }
 }
-
