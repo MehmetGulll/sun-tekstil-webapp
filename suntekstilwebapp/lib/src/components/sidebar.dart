302f088d221b54f3a92e1bd5656ba9519a8d02cc
Atakan <atakandogan2001@gmail.com>
2024-04-01 13:33:27 +0300

    Update sidebar.dart

--- suntekstilwebapp/lib/src/components/sidebar.dart
+++ suntekstilwebapp/lib/src/components/sidebar.dart
@@ -1,44 +1,52 @@
 import 'package:flutter/material.dart';
-import '../constants/theme.dart';
-import '../constants/tokens.dart';
+import 'package:suntekstilwebapp/src/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
-class Sidebar extends StatelessWidget {
+class Sidebar extends StatefulWidget {
+  @override
+  _SidebarState createState() => _SidebarState();
+}
+
+class _SidebarState extends State<Sidebar> {
+  late IconData _iconData;
+
+  @override
+  void initState() {
+    super.initState();
+    _updateIconData();
+  }
+
+  void _updateIconData() {
+    ThemeProvider themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+    _iconData = themeProvider.themeType == ThemeType.Dark ? Icons.dark_mode : Icons.light_mode;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(
-        color: Themes.blackColor,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Themes.whiteColor,
-                ),
-                child: Align(
-                    alignment: Alignment.center,
-                    child: Image.network(
-                        'https://static.jimmykey.com/Images/JMK/jimmy_logo_black_1.png'))),
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text(
-                'Ana Sayfa',
-                style: TextStyle(color: Themes.whiteColor),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text(
+              'Sidebar Başlık',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
               ),
-              onTap: () {
-                Navigator.pop(context);
-                if (ModalRoute.of(context)?.settings.name != '/') {
-                  Navigator.pushReplacementNamed(context, '/');
-                }
-              },
             ),
           ),
           ListTile(
             leading: Icon(Icons.home),
-            title: Text('Ana Sayfaaaaaa'),
+            title: Text('Ana Sayfa'),
             onTap: () {
               Navigator.pop(context); 
-              Navigator.pushNamed(context, '/'); 
+              if (ModalRoute.of(context)?.settings.name != '/') {
+                Navigator.pushReplacementNamed(context, '/');
+              }
             },
           ),
           ListTile(
@@ -46,7 +54,9 @@ class Sidebar extends StatelessWidget {
             title: Text('Ayarlar'),
             onTap: () {
               Navigator.pop(context);
-              Navigator.pushNamed(context, '/settings');
+              if (ModalRoute.of(context)?.settings.name != '/settings') {
+                Navigator.pushReplacementNamed(context, '/settings');
+              }
             },
           ),
           ExpansionTile(
@@ -58,54 +68,42 @@ class Sidebar extends StatelessWidget {
                 title: Text('PDF Dosyaları'),
                 onTap: () {
                   Navigator.pop(context);
+                  // PDF dosyaları sayfasına gitmek için yönlendirme işlemleri
                 },
               ),
               ListTile(
                 leading: Icon(Icons.image),
                 title: Text('Resim Dosyaları'),
                 onTap: () {
-                  Navigator.pop(context); 
+                  Navigator.pop(context);
+                  // Resim dosyaları sayfasına gitmek için yönlendirme işlemleri
                 },
-            ListTile(
-              leading: Icon(Icons.settings),
-              title: Text(
-                'Ayarlar',
-                style: TextStyle(color: Themes.whiteColor),
               ),
-              onTap: () {
-                Navigator.pop(context);
-                if (ModalRoute.of(context)?.settings.name != '/settings') {
-                  Navigator.pushReplacementNamed(context, '/settings');
+            ],
+          ),
+          ListTile(
+            title: Text('Tema Değiştir'),
+            trailing: IconButton(
+              icon: Icon(_iconData),
+              onPressed: () {
+                ThemeProvider themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+                // Tema değişikliği yapılıyor
+                if (themeProvider.themeType == ThemeType.System && MediaQuery.of(context).platformBrightness == Brightness.dark) {
+                  // Sistem teması karanlıksa ve tema varsayılan olarak sistem temasına ayarlı ise, aydınlık temaya geçiş yap
+                  themeProvider.setThemeType(ThemeType.Light);
+                } else {
+                  // Diğer durumlarda tema değiştirme işlevini uygula
+                  themeProvider.setThemeType(
+                    themeProvider.themeType == ThemeType.Dark ? ThemeType.Light : ThemeType.Dark,
+                  );
                 }
+                setState(() {
+                  _updateIconData(); // Icon data güncelleniyor
+                });
               },
             ),
-            ExpansionTile(
-              leading: Icon(Icons.folder),
-              title:
-                  Text('Dosyalar', style: TextStyle(color: Themes.whiteColor)),
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.picture_as_pdf),
-                  title: Text(
-                    'PDF Dosyaları',
-                    style: TextStyle(color: Themes.whiteColor),
-                  ),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                ListTile(
-                  leading: Icon(Icons.image),
-                  title: Text('Resim Dosyaları',
-                      style: TextStyle(color: Themes.whiteColor)),
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
