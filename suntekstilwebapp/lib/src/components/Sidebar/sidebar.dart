795b6faca9f4cafa22a6d3fc75af291b65f4b335
Mehmet Rıdvan Gül <mgul440@hotmail.com>
2024-04-04 20:43:36 +0300

    update: sidebar new code

---
+++ suntekstilwebapp/lib/src/components/Sidebar/sidebar.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import '../../constants/theme.dart';
+
+class Sidebar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final drawerHeader = DrawerHeader(
+      decoration: BoxDecoration(
+        color: Themes.whiteColor,
+      ),
+      child: Align(
+          alignment: Alignment.center,
+          child: Image.network(
+              'https://static.jimmykey.com/Images/JMK/jimmy_logo_black_1.png')),
+    );
+    final homeTile = ListTile(
+      leading: Icon(Icons.home),
+      title: Text(
+        'Ana Sayfa',
+        style: TextStyle(color: Themes.whiteColor),
+      ),
+      onTap: () {
+        Navigator.pop(context);
+        if (ModalRoute.of(context)?.settings.name != '/') {
+          Navigator.pushReplacementNamed(context, '/');
+        }
+      },
+    );
+
+    final settingsTile = ListTile(
+      leading: Icon(Icons.settings),
+      title: Text(
+        'Ayarlar',
+        style: TextStyle(color: Themes.whiteColor),
+      ),
+      onTap: () {
+        Navigator.pop(context);
+        if (ModalRoute.of(context)?.settings.name != '/settings') {
+          Navigator.pushReplacementNamed(context, '/settings');
+        }
+      },
+    );
+
+    final filesExpansionTile = ExpansionTile(
+      leading: Icon(Icons.folder),
+      title: Text('Dosyalar', style: TextStyle(color: Themes.whiteColor)),
+      children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.picture_as_pdf),
+          title: Text(
+            'PDF Dosyaları',
+            style: TextStyle(color: Themes.whiteColor),
+          ),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+        ListTile(
+          leading: Icon(Icons.image),
+          title: Text('Resim Dosyaları',
+              style: TextStyle(color: Themes.whiteColor)),
+          onTap: () {
+            Navigator.pop(context);
+          },
+        ),
+      ],
+    );
+
+    return Drawer(
+      child: Container(
+        color: Themes.blackColor,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            drawerHeader,
+            homeTile,
+            settingsTile,
+            filesExpansionTile,
+          ],
+        ),
+      ),
+    );
+  }
+}
