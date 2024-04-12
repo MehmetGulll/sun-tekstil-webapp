5d53521336058bc094cd6a3f504b69913ae3ed2a
Mehmet Rıdvan Gül <mgul440@hotmail.com>
2024-04-12 23:43:49 +0300

    update: modal component

--- suntekstilwebapp/lib/src/components/Modal/Modal.dart
+++ suntekstilwebapp/lib/src/components/Modal/Modal.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 class CustomModal extends StatelessWidget {
   final Color backgroundColor;
   final String text;
+  final Widget child;
 
-  CustomModal({
-    required this.backgroundColor,
-    required this.text,
-  });
+  CustomModal(
+      {required this.backgroundColor, required this.text, required this.child});
 
   @override
   Widget build(BuildContext context) {
@@ -15,14 +14,17 @@ class CustomModal extends StatelessWidget {
       backgroundColor: backgroundColor,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Padding(
-          padding: const EdgeInsets.all(150.0), 
-          child: Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
+        child: SingleChildScrollView(
+          child: Container(
+              child: Column(
+            children: [
+              Text(
+                text,
+                style: TextStyle(color: Colors.white),
+              ),
+              child
+            ],
+          )),
         ),
       ),
     );
